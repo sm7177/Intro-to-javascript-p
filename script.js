0fd9b1544480aa63b2785c2c67ed92300290b16c
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-05 08:05:44 +0545

    project-intro to js

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("hello")
+var fname=prompt("Enter your name")
+var a=+prompt("Enter a")
+var b=+prompt("Enter b")
+var c=+prompt("Enter c")
+var addition=a+b+c
+var subtraction=a-b-c
+var multiplication=a*b*c
+var division=a/b/c
+var reminder=a%c
+
+document.getElementById("fname").innerHTML="Hello "+fname 
+document.getElementById("add").innerHTML="The addition of a and b and c is "+addition
+document.getElementById("subs").innerHTML="The subtraction of a and b and c is "+subtraction
+document.getElementById("mult").innerHTML="The multiplication of a and b and c is "+multiplication
+document.getElementById("divs").innerHTML="The division of a and b and c is "+division
+document.getElementById("mod").innerHTML="The reminder of a and c is "+reminder
+
+console.log("addition")
+console.log("hello")
+
+
